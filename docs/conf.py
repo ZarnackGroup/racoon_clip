550584a528167f805b2b6694a3e57e438f5d168f
MelinaKlostermann <67367432+MelinaKlostermann@users.noreply.github.com>
2023-08-23 15:32:00 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -24,5 +24,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "master"
-html_static_path = ['_static']
+#html_theme = "master"
+#html_static_path = ['_static']
