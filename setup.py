28df4e81baf7ef38fe7cc065362319560c6d6ff4
MelinaKlostermann <67367432+MelinaKlostermann@users.noreply.github.com>
2023-10-24 11:30:36 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -54,8 +54,7 @@ setup(
     install_requires=[
         "snakemake>=7.22",
         "pyyaml>=6.0",
-        "Click>=8.1.3",
-        "mamba"
+        "Click>=8.1.3"
     ],
     entry_points={
         "console_scripts": [
