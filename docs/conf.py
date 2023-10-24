35fa787d11d1fb74b1b4716eeeec80713b076f43
MelinaKlostermann <67367432+MelinaKlostermann@users.noreply.github.com>
2023-10-24 10:00:57 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -29,6 +29,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
+html_show_sourcelink = False
 html_static_path = ['_static']
 html_logo = '../Racoon_Logo_Schrift.png'
 html_theme_options = {
