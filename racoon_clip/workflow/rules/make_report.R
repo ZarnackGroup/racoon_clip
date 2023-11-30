70bd0975cca132ae1ddb8b642bbc705623750de8
MelinaKlostermann <melinaklostermann@googlemail.com>
2023-11-30 15:17:53 +0000

    - add r-cairo to r environment - fix breaking Rmarkdown report at deduplication chunk

--- racoon_clip/workflow/rules/make_report.R
+++ racoon_clip/workflow/rules/make_report.R
@@ -1,11 +1,15 @@
 # find_pandoc(cache = TRUE, dir = /home/mklostermann/applications/anaconda3/envs/racoon_r_fix1/bin/pandoc, version = NULL)
 #Sys.setenv(RSTUDIO_PANDOC="--- insert directory here ---")
 
-pre_c <- read.delim(snakemake@input[[2]], header = F)
+#pre_c <- read.delim(snakemake@input[[2]], header = F)
 #pre_c[1, 1]
 
 #paste0(pre_c[1,1], "/bin/pandoc")
-rmarkdown::find_pandoc(dir = paste0(pre_c[1,1], "/bin/"))
+#rmarkdown::find_pandoc(dir = paste0(pre_c[1,1], "/bin/"))
+
+#https://community.rstudio.com/t/cannot-open-file-myfile-knit-md-read-only-file-system/59941
+report_path <- tempfile(fileext = ".Rmd")
+file.copy("report.Rmd", report_path, overwrite = TRUE)
 
 rmarkdown::render(paste0(snakemake@params[[3]], "/workflow/rules/Report.rmd"),
   output_dir=paste0(snakemake@params[[1]],"/results/"),
